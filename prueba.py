148360b1616e6d831b2e569e73fbc3a7dbfd7328
vrojasc <victor.rojas235@gmail.com>
2019-10-12 02:44:00 +0200

    ascs

--- prueba.py
+++ prueba.py
@@ -1,9 +1,17 @@
 import requests as r
+import json
 
 
 #response = r.get('https://api.fib.upc.edu/v2/o/authorize',headers={'accept': 'application/json'})
-response = r.get('https://api.fib.upc.edu/v2/o/authorize',headers={'accept': 'application/json'})
-print(response.json())
+params = dict(
+    client_id = 'KJ0fFNkeNeTRDKOLRFhsI5nW67w0tZItlgP8909',
+    response_type = 'code',
+    scope = 'read',
+    state = 'random_state_string',
+    approval_prompt = 'auto'
+)
+response = r.get('https://api.fib.upc.edu/v2/o/authorize?',headers={'accept': 'application/json'},params=params)
+print(response.content)
 #
 #fib = oauth.remote_app(
 #    'fib',
